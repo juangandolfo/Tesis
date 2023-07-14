b10dd4ce74f0584b76b4d0ecadbc4c8a80eaa54a
juangandolfo <89227103+juangandolfo@users.noreply.github.com>
2023-07-14 00:23:27 -0300

    completo varialbles, reorganizo codigo, agrego box

--- Visualizacion prueba.py
+++ Visualizacion prueba.py
@@ -36,28 +36,21 @@ class GraphApp:
     
     NumDataPts2Display=SampleFreq*Sec2Display
     NumDataPts2Save = SampleFreq*Sec2Save
-
+    
     def __init__(self, root):
         
         self.root = root
-        self.root.title("Random Points Visualization")
+        self.root.title("Muscle and synergies activations visualization")
         self.fig = plt.figure()
-        gs = gridspec.GridSpec(1, 2, width_ratios=[2, 1.5])  # Create a 1x2 grid with different width ratios
-
-        self.ax = self.fig.add_subplot(gs[0])  # Add first graph to the left column
-        self.ax2 = self.fig.add_subplot(gs[1])  # Add second graph to the right column
-
+        
+        gs = gridspec.GridSpec(nrows=1, ncols=2, width_ratios=[1, 6])
+        
+        #agrego los subplots a la ventana
+        self.ax = self.fig.add_subplot(gs[:,0])  # Add first graph to the left column
+        self.ax2 = self.fig.add_subplot(gs[:,1])  # Add second graph to the right column
+        
         # Code for the first Graph
-        #self.ax = self.fig.add_subplot(111)
-
-        self.muscle1 = []
-        self.muscle2 = []
-        self.muscle3 = []
-        self.current_x = 0
-
-        self.history1 = []
-        self.history2 = []
-        self.history3 = []
+        self.initVariables()
 
         # Code for the second Graph
 
@@ -68,43 +61,11 @@ class GraphApp:
         # Code for the checkboxes
 
         self.canvas = FigureCanvasTkAgg(self.fig, master=root)
-        self.canvas.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-
-        #creo las variables para mostrar o no los musculos y sinergias
-        self.show_muscle1 = tk.BooleanVar()
-        self.show_muscle2 = tk.BooleanVar()
-        self.show_muscle3 = tk.BooleanVar()
-        self.show_muscle4 = tk.BooleanVar()
-        self.show_muscle5 = tk.BooleanVar()
-        self.show_muscle6 = tk.BooleanVar()
-        self.show_muscle7 = tk.BooleanVar()
-        self.show_muscle8 = tk.BooleanVar()
-        self.show_synergy1 = tk.BooleanVar()
-        self.show_synergy2 = tk.BooleanVar()
-        self.show_synergy3 = tk.BooleanVar()
-        self.show_synergy4 = tk.BooleanVar()
-        self.show_synergy5 = tk.BooleanVar()
-
-        self.show_PrintHistory1 = tk.BooleanVar()
-        
-        #initialize the variables created
-        self.show_muscle1.set(True)
-        self.show_muscle2.set(True)
-        self.show_muscle3.set(True)
-        self.show_muscle4.set(True)
-        self.show_muscle5.set(True)
-        self.show_muscle6.set(True)
-        self.show_muscle7.set(True)
-        self.show_muscle8.set(True)
-        self.show_synergy1.set(True) 
-        self.show_synergy2.set(True)
-        self.show_synergy3.set(True) 
-        self.show_synergy4.set(True)
-        self.show_synergy5.set(True)
-        
-        self.show_PrintHistory1.set(False)
+        self.canvas.get_tk_widget().pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
 
         #Create the checkbutton and link it to variable
+        Title1 = tk.Label(root, text="MUSCLES")
+        Title1.pack()
         self.muscle1_checkbox = tk.Checkbutton(root, text='Show Muscle 1', variable=self.show_muscle1, command=self.update_line_visibility)
         self.muscle1_checkbox.pack(anchor=tk.W)
         self.muscle2_checkbox = tk.Checkbutton(root, text='Show Muscle 2', variable=self.show_muscle2, command=self.update_line_visibility)
@@ -121,6 +82,11 @@ class GraphApp:
         self.muscle7_checkbox.pack(anchor=tk.W)
         self.muscle8_checkbox = tk.Checkbutton(root, text='Show Muscle 8', variable=self.show_muscle8, command=self.update_line_visibility)
         self.muscle8_checkbox.pack(anchor=tk.W)
+        space1 = tk.Label(root, text="\n")
+        space1.pack()
+        
+        Title2 = tk.Label(root, text="SYNERGIES")
+        Title2.pack()
         self.synergy1_checkbox = tk.Checkbutton(root, text='Show Synergy 1', variable=self.show_synergy1, command=self.update_line_visibility)
         self.synergy1_checkbox.pack(anchor=tk.W)
         self.synergy2_checkbox = tk.Checkbutton(root, text='Show Synergy 2', variable=self.show_synergy2, command=self.update_line_visibility)
@@ -131,10 +97,17 @@ class GraphApp:
         self.synergy4_checkbox.pack(anchor=tk.W)
         self.synergy5_checkbox = tk.Checkbutton(root, text='Show Synergy 5', variable=self.show_synergy5, command=self.update_line_visibility)
         self.synergy5_checkbox.pack(anchor=tk.W)
+        space2 = tk.Label(root, text="\n")
+        space2.pack()
 
-        self.printHistory1_checkbox = tk.Checkbutton(root, text='printHistory1', variable=self.show_PrintHistory1, command=self.update_line_visibility)
-        self.printHistory1_checkbox.pack(anchor=tk.W)
-
+        Title3 = tk.Label(root, text="PRINT TO CONSOLE")
+        Title3.pack()
+        self.printHistory1_button = tk.Button(root, text='M1H', command=self.print_Muscle1History)
+        self.printHistory1_button.pack()
+        self.printHistory2_button = tk.Button(root, text='M2H', command=self.print_Muscle2History)
+        self.printHistory2_button.pack()
+        
+        
         self.data_thread = DataCollectionThread(self)
         self.data_thread.start()
 
@@ -151,39 +124,39 @@ class GraphApp:
         point3 = (self.current_x, y3)
         
         #append to active display list and history list
-        self.muscle1.append(point1)
-        self.muscle2.append(point2)
-        self.muscle3.append(point3)
-        self.history1.append(point1)
-        self.history2.append(point2)
-        self.history3.append(point3)
+        self.Muscle1.append(point1)
+        self.Muscle2.append(point2)
+        self.Muscle3.append(point3)
+        self.Muscle1History.append(point1)
+        self.Muscle2History.append(point2)
+        self.Muscle3History.append(point3)
 
         #delete old data
-        if len(self.muscle1) > self.NumDataPts2Display:
-            self.muscle1 = self.muscle1[1:]
-        if len(self.muscle2) > self.NumDataPts2Display:
-            self.muscle2 = self.muscle2[1:]
-        if len(self.muscle3) > self.NumDataPts2Display:
-            self.muscle3 = self.muscle3[1:]
+        if len(self.Muscle1) > self.NumDataPts2Display:
+            self.Muscle1 = self.Muscle1[1:]
+        if len(self.Muscle2) > self.NumDataPts2Display:
+            self.Muscle2 = self.Muscle2[1:]
+        if len(self.Muscle3) > self.NumDataPts2Display:
+            self.Muscle3 = self.Muscle3[1:]
 
-        if len(self.history1) > self.NumDataPts2Save:
-            self.history1 = self.history1[1:]
-        if len(self.history2) > self.NumDataPts2Save:
-            self.history2 = self.history2[1:]
-        if len(self.history3) > self.NumDataPts2Save:
-            self.history3 = self.history3[1:]
+        if len(self.Muscle1History) > self.NumDataPts2Save:
+            self.Muscle1History = self.Muscle1History[1:]
+        if len(self.Muscle2History) > self.NumDataPts2Save:
+            self.Muscle2History = self.Muscle2History[1:]
+        if len(self.Muscle3History) > self.NumDataPts2Save:
+            self.Muscle3History = self.Muscle3History[1:]
 
         self.current_x += 1
 
     def update_graph(self):
         self.ax.clear()
 
-        x_values1 = [point[0] for point in self.muscle1] #cada punto trae [numMuestra,muestra]
-        y_values1 = [point[1] for point in self.muscle1]
-        x_values2 = [point[0] for point in self.muscle2]
-        y_values2 = [point[1] for point in self.muscle2]
-        x_values3 = [point[0] for point in self.muscle3]
-        y_values3 = [point[1] for point in self.muscle3]
+        x_values1 = [point[0] for point in self.Muscle1] #cada punto trae [numMuestra,muestra]
+        y_values1 = [point[1] for point in self.Muscle1]
+        x_values2 = [point[0] for point in self.Muscle2]
+        y_values2 = [point[1] for point in self.Muscle2]
+        x_values3 = [point[0] for point in self.Muscle3]
+        y_values3 = [point[1] for point in self.Muscle3]
 
 
         if self.show_muscle1.get():
@@ -192,18 +165,6 @@ class GraphApp:
             self.ax.plot(x_values2, y_values2, 'blue', label='Blue Graph')
         if self.show_muscle3.get():
             self.ax.plot(x_values3, y_values3, 'green', label='Green Graph')
-        if self.show_PrintHistory1.get():
-            print(self.muscle1)
-            print(self.history1)
-            '''
-            last_filtered_sample1 = self.apply_low_pass_filter(x_values1, y_values1, 100)
-            last_filtered_sample2 = self.apply_low_pass_filter(x_values2, y_values2, 100)
-            last_filtered_sample3 = self.apply_low_pass_filter(x_values3, y_values3, 100)
-
-            print(last_filtered_sample1)
-            print(last_filtered_sample2)
-            print(last_filtered_sample3)'''
-            self.show_PrintHistory1.set(False)
 
         self.ax.set_xlim(self.current_x - 1000, self.current_x)
         self.ax.set_ylim(0, 1)
@@ -215,9 +176,9 @@ class GraphApp:
         # Create the bar graph for the second graph
         #tomando la ultima muestra
         
-        last_sample1 = self.muscle1[-1][1] if self.muscle1 else 0
-        last_sample2 = self.muscle2[-1][1] if self.muscle2 else 0
-        last_sample3 = self.muscle3[-1][1] if self.muscle3 else 0
+        last_sample1 = self.Muscle1[-1][1] if self.Muscle1 else 0
+        last_sample2 = self.Muscle2[-1][1] if self.Muscle2 else 0
+        last_sample3 = self.Muscle3[-1][1] if self.Muscle3 else 0
 
         #tomando con LPF
         '''last_sample1 = self.apply_low_pass_filter(x_values1, y_values1, 50) if self.muscle1 else 0
@@ -293,6 +254,109 @@ class GraphApp:
         last_filtered_sample = filtered_y[-1]
 
         return last_filtered_sample
+
+    def print_Muscle1History(self):
+        formatted_list = '[{}]'.format('\n'.join('   '.join(map(str, item)) for item in self.Muscle1History))
+        print(formatted_list)
+    
+    def print_Muscle2History(self):
+        print(self.Muscle2History)
+        
+    def print_Muscle3History(self):
+        print(self.Muscle1History)
+        
+    def print_Muscle4History(self):
+        print(self.Muscle1History)
+        
+    def print_Muscle5History(self):
+        print(self.Muscle1History)
+        
+    def print_Muscle6History(self):
+        print(self.Muscle1History)
+        
+    def print_Muscle7History(self):
+        print(self.Muscle1History)
+        
+    def print_Muscle8History(self):
+        print(self.Muscle1History)
+        
+    def print_Synergy1History(self):
+        print(self.Muscle1History)
+        
+    def print_Synergy2History(self):
+        print(self.Muscle1History)
+        
+    def print_Synergy3History(self):
+        print(self.Muscle1History)
+        
+    def print_Synergy4History(self):
+        print(self.Muscle1History)
+        
+    def print_Synergy5History(self):
+        print(self.Muscle1History)
+
+    def initVariables(self):
+        #create muscle and synergies lists
+        self.current_x = 0
+
+        self.Muscle1 = []
+        self.Muscle2 = []
+        self.Muscle3 = []
+        self.Muscle4 = []
+        self.Muscle5 = []
+        self.Muscle6 = []
+        self.Muscle7 = []
+        self.Muscle8 = []
+        self.Synergy1 = []
+        self.Synergy2 = []
+        self.Synergy3 = []
+        self.Synergy4 = []
+        self.Synergy5 = []
+
+        self.Muscle1History = []
+        self.Muscle2History = []
+        self.Muscle3History = []
+        self.Muscle4History = []
+        self.Muscle5History = []
+        self.Muscle6History = []
+        self.Muscle7History = []
+        self.Muscle8History = []
+        self.Synergy1History = []
+        self.Synergy2History = []
+        self.Synergy3History = []
+        self.Synergy4History = []
+        self.Synergy5History = []
+
+        #create and init variables for the checkboxes    
+                
+        self.show_muscle1 = tk.BooleanVar()
+        self.show_muscle2 = tk.BooleanVar()
+        self.show_muscle3 = tk.BooleanVar()
+        self.show_muscle4 = tk.BooleanVar()
+        self.show_muscle5 = tk.BooleanVar()
+        self.show_muscle6 = tk.BooleanVar()
+        self.show_muscle7 = tk.BooleanVar()
+        self.show_muscle8 = tk.BooleanVar()
+        self.show_synergy1 = tk.BooleanVar()
+        self.show_synergy2 = tk.BooleanVar()
+        self.show_synergy3 = tk.BooleanVar()
+        self.show_synergy4 = tk.BooleanVar()
+        self.show_synergy5 = tk.BooleanVar()
+        
+        #initialize the variables created
+        self.show_muscle1.set(True)
+        self.show_muscle2.set(True)
+        self.show_muscle3.set(True)
+        self.show_muscle4.set(True)
+        self.show_muscle5.set(True)
+        self.show_muscle6.set(True)
+        self.show_muscle7.set(True)
+        self.show_muscle8.set(True)
+        self.show_synergy1.set(True) 
+        self.show_synergy2.set(True)
+        self.show_synergy3.set(True) 
+        self.show_synergy4.set(True)
+        self.show_synergy5.set(True)
 
 if __name__ == "__main__":
     root = tk.Tk()
